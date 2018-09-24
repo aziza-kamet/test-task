7c3fdc5383fb9f06dcca28c823eb749ff765728c
Aziza Kamet <aziza.kamet@gmail.com>
2018-09-25 00:16:07 +0600

    refactor(csv): move csv export to separate class

--- app/controllers/PromotionController.php
+++ app/controllers/PromotionController.php
@@ -2,34 +2,18 @@
 namespace app\controllers;
 
 
+use app\helpers\CSVExport;
 use app\models\Promotion;
 
 class PromotionController
 {
     public function index()
     {
-        if (($handle = fopen("web/files/test.csv", "r")) !== false) {
-
-            Promotion::truncate();
-            while (($data = fgetcsv($handle, 1000, ";")) !== false) {
-                if ($data[0] === 'ID акции') {
-                    continue;
-                }
-
-                $promotion = new Promotion();
-                $promotion->id = $data[0];
-                $promotion->name = $data[1];
-                $promotion->startDate = strtotime($data[2]);
-                $promotion->endDate = strtotime($data[3]);
-                $promotion->status = $promotion->statusValue($data[4]);
-                $promotion->save();
-            }
-            fclose($handle);
-        }
+        CSVExport::export();
 
         $promotion = Promotion::selectRandom();
         $promotion->status = intval(!$promotion->status);
         $promotion->save();
-        var_dump(implode('; ', $promotion->columnMap()));
+        var_dump(implode(CSVExport::DELIMITER . ' ', $promotion->columnMap()));
     }
 }
---
+++ app/helpers/CSVExport.php
@@ -0,0 +1,34 @@
+<?php
+namespace app\helpers;
+
+
+use app\models\Promotion;
+
+class CSVExport
+{
+    private const FILE_PATH = 'web/files/test.csv';
+    private const COLUMN_NAME = 'ID акции';
+    public const DELIMITER = ';';
+
+    public static function export()
+    {
+        if (($handle = fopen(self::FILE_PATH, 'r')) !== false) {
+
+            Promotion::truncate();
+            while (($data = fgetcsv($handle, 1000, self::DELIMITER)) !== false) {
+                if ($data[0] === self::COLUMN_NAME) {
+                    continue;
+                }
+
+                $promotion = new Promotion();
+                $promotion->id = $data[0];
+                $promotion->name = $data[1];
+                $promotion->startDate = strtotime($data[2]);
+                $promotion->endDate = strtotime($data[3]);
+                $promotion->status = $promotion->statusValue($data[4]);
+                $promotion->save();
+            }
+            fclose($handle);
+        }
+    }
+}
